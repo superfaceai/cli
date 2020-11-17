8e12573afdb7614373e60256e60599b47b47f6f4
TheEdward162 <TheEdward162@gmail.com>
2020-11-17 19:35:18 +0100

    chore: lint + format

--- src/common/io.ts
+++ src/common/io.ts
@@ -3,44 +3,43 @@ import { Writable } from 'stream';
 import { promisify } from 'util';
 
 export const readFilePromise = promisify(fs.readFile);
-export function streamWritePromise(stream: Writable, data: string): Promise<void> {
-  return new Promise(
-    (resolve, reject) => {
-      const writeMore = stream.write(data, 'utf-8');
-      
-      if (!writeMore) {
-        stream.once('error', reject);
-        stream.once('drain', resolve);
-      } else {
-        resolve();
-      }
+export function streamWritePromise(
+  stream: Writable,
+  data: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const writeMore = stream.write(data, 'utf-8');
+
+    if (!writeMore) {
+      stream.once('error', reject);
+      stream.once('drain', resolve);
+    } else {
+      resolve();
     }
-  )
+  });
 }
 export function streamEndPromise(stream: Writable): Promise<void> {
-  return new Promise(
-    (resolve, reject) => {
-      stream.once('error', reject);
-      stream.once('close', resolve);
-    }
-  )
+  return new Promise((resolve, reject) => {
+    stream.once('error', reject);
+    stream.once('close', resolve);
+  });
 }
 
 export class OutputStream {
   readonly stream: Writable;
-  
+
   readonly isStdStream: boolean;
   readonly isTTY: boolean;
 
   /**
    * Constructs the output object.
-   * 
+   *
    * `path` accepts 2 special values:
    * * `-` - initializes output for stdout
    * * `-2` - initializes output for stderr
-   * 
+   *
    * All other `path` values are treated as file system path.
-   * 
+   *
    * When `append` is true the file at `path` is opened in append mode rather than in write (truncate) mode.
    */
   constructor(path: string, append?: boolean) {
@@ -61,7 +60,7 @@ export class OutputStream {
         this.stream = fs.createWriteStream(path, {
           flags: append ? 'a' : 'w',
           mode: 0o644,
-          encoding: 'utf-8'
+          encoding: 'utf-8',
         });
         this.isStdStream = true;
         this.isTTY = process.stdout.isTTY;
