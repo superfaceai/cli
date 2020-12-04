f841644f49309a1187a48efe984468317e6af897
Martin Albert <martin.albert.187@gmail.com>
2020-12-04 15:44:36 +0100

    test: profile-map-validator integration

--- src/commands/lint.test.ts
+++ src/commands/lint.test.ts
@@ -79,11 +79,13 @@ describe('lint CLI command', () => {
 
     expect(result.reports).toContainEqual({
       path: './fixtures/strict.suma',
+      kind: 'file',
       errors: [],
       warnings: [],
     });
     expect(result.reports).toContainEqual({
       path: './fixtures/invalid.suma',
+      kind: 'file',
       errors: [
         {
           category: 1,
@@ -107,5 +109,23 @@ describe('lint CLI command', () => {
 
     expect(stderr.output).toContain('🆗 ./fixtures/strict.supr\n' + '\n');
     expect(stderr.output).toContain('Detected 0 problems\n');
+  });
+
+  it('lints multiple maps to specific profile', async () => {
+    await expect(
+      Lint.run(['-v', './fixtures/testProfile.supr', './fixtures/testMap.suma'])
+    ).rejects.toHaveProperty(['oclif', 'exit'], 1);
+
+    expect(stdout.output).toContain('❌ ./fixtures/testMap.suma');
+    expect(stdout.output).toContain(
+      '8:1 ProfileId - Wrong Profile ID: expected https://example.com/profile/myProfile, but got http://example.com/profile'
+    );
+    expect(stdout.output).toContain(
+      '20:19 InlineCall - Operation not found: Op'
+    );
+    expect(stdout.output).toContain(
+      '90:14 ObjectLiteral - Wrong Structure: expected 404 or 400, but got "ObjectLiteral"'
+    );
+    expect(stdout.output).toContain('Detected 12 problems');
   });
 });
