fd970a90aec9a6420446f7633a9b91a79e255384
Martin Albert <martin.albert.187@gmail.com>
2021-01-22 15:31:42 +0100

    feat: implement init command

---
+++ src/commands/init.ts
@@ -0,0 +1,40 @@
+import { Command, flags } from '@oclif/command';
+import { grey } from 'chalk';
+
+import { initSuperface } from '../logic/init';
+
+export default class Init extends Command {
+  static description = 'Initializes superface local folder structure.';
+
+  static flags = {
+    quiet: flags.boolean({
+      char: 'q',
+      description:
+        'When set to true, disables the shell echo output of play actions.',
+      default: false,
+    }),
+    help: flags.help({ char: 'h' }),
+  };
+
+  static args = [
+    {
+      name: 'path',
+      description: 'Path where to initialize folder structure.',
+      required: false,
+    },
+  ];
+
+  private logCallback? = (message: string) => this.log(grey(message));
+
+  async run(): Promise<void> {
+    const { args, flags } = this.parse(Init);
+
+    if (flags.quiet) {
+      this.logCallback = undefined;
+    }
+
+    await initSuperface(args.path ?? './', {}, {}, {
+      logCb: this.logCallback,
+    });
+  }
+}
