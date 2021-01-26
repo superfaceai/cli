b58f6a9fdd49d5ededa3e4da4a6a3b8740cb37ec
Martin Albert <martin.albert.187@gmail.com>
2021-01-26 19:18:01 +0100

    refactor: seperate lint logic and refactor -v flag - move business logic into logic module - refactor use of -v flag   - allow only validated maps to be linted     - validated map has valid header according to given profile header

--- src/commands/lint.ts
+++ src/commands/lint.ts
@@ -1,57 +1,19 @@
 import { Command, flags } from '@oclif/command';
-import { CLIError } from '@oclif/errors';
-import {
-  formatIssues,
-  getProfileOutput,
-  MapDocumentId,
-  ProfileDocumentId,
-  Source,
-  SyntaxError,
-  validateMap,
-  ValidationIssue,
-} from '@superfaceai/parser';
 
-import {
-  DOCUMENT_PARSE_FUNCTION,
-  DocumentType,
-  inferDocumentTypeWithFlag,
-  isMapFile,
-  isProfileFile,
-  isUnknownFile,
-} from '../common/document';
 import { developerError, userError } from '../common/error';
 import { DocumentTypeFlag, documentTypeFlag } from '../common/flags';
-import { OutputStream, readFile } from '../common/io';
+import { OutputStream } from '../common/io';
+import { ReportFormat } from '../common/report.interfaces';
 import {
-  getMapDocument,
-  getMapHeader,
-  getProfileDocument,
-  getProfileHeader,
+  formatHuman,
+  formatJson,
+  lintFiles,
+  lintMapsToProfile,
 } from '../logic/lint';
 import { formatWordPlurality } from '../util';
 
-type ReportKind = 'file' | 'compatibility';
-
-interface Report {
-  kind: ReportKind;
-  path: string;
-}
-
-interface FileReport extends Report {
-  kind: 'file';
-  errors: SyntaxError[];
-  warnings: unknown[];
-}
-
-interface ProfileMapReport extends Report {
-  kind: 'compatibility';
-  errors: ValidationIssue[];
-  warnings: ValidationIssue[];
-}
-
-type ReportFormat = FileReport | ProfileMapReport;
-
 type OutputFormatFlag = 'long' | 'short' | 'json';
+
 export default class Lint extends Command {
   static description =
     'Lints a map or profile file. Outputs the linter issues to STDOUT by default.\nLinter ends with non zero exit code if errors are found.';
@@ -131,7 +93,7 @@ export default class Lint extends Command {
             flags.validate,
             '\n',
             report =>
-              Lint.formatHuman(
+              formatHuman(
                 report,
                 flags.quiet,
                 flags.outputFormat === 'short',
@@ -156,7 +118,7 @@ export default class Lint extends Command {
             flags.documentType,
             flags.validate,
             ',',
-            report => Lint.formatJson(report)
+            report => formatJson(report)
           );
           await outputStream.write(
             `],"total":{"errors":${totals[0]},"warnings":${totals[1]}}}\n`
@@ -182,232 +144,24 @@ export default class Lint extends Command {
     outputGlue: string,
     fn: (report: ReportFormat) => string
   ): Promise<[errors: number, warnings: number]> {
-    let outputCounter = files.length;
-    let counts: [number, number][] = [];
-
-    if (!validateFlag) {
-      counts = await Promise.all(
-        files.map(
-          async (file): Promise<[number, number]> => {
-            const report = await Lint.lintFile(file, documentTypeFlag);
-
-            let output = fn(report);
-            if (outputCounter > 1) {
-              output += outputGlue;
-            }
-            outputCounter -= 1;
-
-            await outputStream.write(output);
-
-            return [report.errors.length, report.warnings.length];
-          }
+    const outputCounter = files.length;
+    const counts = validateFlag
+      ? await lintMapsToProfile(
+          files,
+          outputStream,
+          outputCounter,
+          outputGlue,
+          fn
         )
-      );
-    } else {
-      counts = await Lint.lintMapsToProfile(
-        files,
-        outputStream,
-        outputCounter,
-        outputGlue,
-        fn
-      );
-    }
-
-    return counts.reduce((acc, curr) => [acc[0] + curr[0], acc[1] + curr[1]]);
-  }
-
-  static async lintFile(
-    path: string,
-    documentTypeFlag: DocumentTypeFlag
-  ): Promise<FileReport> {
-    const documenType = inferDocumentTypeWithFlag(documentTypeFlag, path);
-    if (documenType === DocumentType.UNKNOWN) {
-      throw userError('Could not infer document type', 3);
-    }
-
-    const parse = DOCUMENT_PARSE_FUNCTION[documenType];
-    const content = await readFile(path).then(f => f.toString());
-    const source = new Source(content, path);
-
-    const result: FileReport = {
-      kind: 'file',
-      path,
-      errors: [],
-      warnings: [],
-    };
-
-    try {
-      parse(source);
-    } catch (e) {
-      result.errors.push(e);
-    }
-
-    return result;
-  }
-
-  static async lintMapsToProfile(
-    files: string[],
-    outputStream: OutputStream,
-    outputCounter: number,
-    outputGlue: string,
-    fn: (report: ReportFormat) => string
-  ): Promise<[number, number][]> {
-    const counts: [number, number][] = [];
-    const profiles = files.filter(isProfileFile);
-    const maps = files.filter(isMapFile);
-    const unknown = files.filter(isUnknownFile);
-
-    if (profiles.length === 0) {
-      throw new CLIError('Cannot validate without profile', { exit: -1 });
-    }
-    if (maps.length === 0) {
-      throw new CLIError('Cannot validate without map', {
-        exit: -1,
-      });
-    }
-
-    if (unknown.length > 0) {
-      for (const file of unknown) {
-        const report: FileReport = {
-          kind: 'file',
-          path: file,
-          errors: [],
-          warnings: ['Could not infer document type'],
-        };
-
-        let output = fn(report);
-        if (unknown.length > 1 && output !== '') {
-          output += outputGlue;
-        }
-
-        await outputStream.write(output);
-      }
-
-      counts.push([0, unknown.length]);
-    }
-
-    const profileHeaders: ProfileDocumentId[] = [];
-    const profileDocuments = [];
-    const mapHeaders: MapDocumentId[] = [];
-
-    for (const profilePath of profiles) {
-      profileDocuments.push(await getProfileDocument(profilePath));
-      profileHeaders.push(await getProfileHeader(profilePath));
-    }
-    for (const mapPath of profiles) {
-      mapHeaders.push(await getMapHeader(mapPath));
-    }
-
-    // loop over profiles and validate only maps that satify condition
-    for (const profile of profileDocuments) {
-      const profileOutput = getProfileOutput(profile);
-
-      for (const mapPath of maps) {
-        // TODO: create condition
-        const result = validateMap(
-          profileOutput,
-          await getMapDocument(mapPath)
+      : await lintFiles(
+          files,
+          outputStream,
+          documentTypeFlag,
+          outputCounter,
+          outputGlue,
+          fn
         );
 
-        const report: ProfileMapReport = result.pass
-          ? {
-              kind: 'compatibility',
-              path: mapPath,
-              errors: [],
-              warnings: result.warnings ?? [],
-            }
-          : {
-              kind: 'compatibility',
-              path: mapPath,
-              errors: result.errors,
-              warnings: result.warnings ?? [],
-            };
-
-        let output = fn(report);
-        if (outputCounter > 1) {
-          output += outputGlue;
-        }
-        outputCounter -= 1;
-
-        await outputStream.write(output);
-
-        counts.push([
-          result.pass ? 0 : result.errors.length,
-          result.warnings?.length ?? 0,
-        ]);
-      }
-    }
-    return counts;
-  }
-
-  private static formatHuman(
-    report: ReportFormat,
-    quiet: boolean,
-    short?: boolean,
-    _color?: boolean
-  ): string {
-    const REPORT_OK = '🆗';
-    const REPORT_WARN = '⚠️';
-    const REPORT_ERR = '❌';
-
-    let prefix;
-    if (report.errors.length > 0) {
-      prefix = REPORT_ERR;
-    } else if (report.warnings.length > 0) {
-      prefix = REPORT_WARN;
-    } else {
-      prefix = REPORT_OK;
-    }
-
-    let buffer = `${prefix} ${report.path}\n`;
-
-    if (prefix === REPORT_WARN && quiet) {
-      return '';
-    }
-
-    if (report.kind === 'file') {
-      for (const error of report.errors) {
-        if (short) {
-          buffer += `\t${error.location.line}:${error.location.column} ${error.message}\n`;
-        } else {
-          buffer += error.format();
-        }
-      }
-      if (report.errors.length > 0 && report.warnings.length > 0) {
-        buffer += '\n';
-      }
-
-      // TODO
-      if (!quiet) {
-        for (const warning of report.warnings) {
-          if (typeof warning === 'string') {
-            buffer += `\t${warning}\n`;
-          }
-        }
-      }
-    } else {
-      buffer += formatIssues(report.errors);
-
-      if (!quiet) {
-        if (report.warnings.length > 0) {
-          buffer += '\n';
-        }
-        buffer += formatIssues(report.warnings);
-      }
-    }
-
-    return buffer;
-  }
-
-  private static formatJson(report: ReportFormat): string {
-    return JSON.stringify(report, (key, value) => {
-      if (key === 'source') {
-        return undefined;
-      }
-
-      // we are just passing the value along, nothing unsafe about that
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return value;
-    });
+    return counts.reduce((acc, curr) => [acc[0] + curr[0], acc[1] + curr[1]]);
   }
 }
--- src/logic/lint.ts
+++ src/logic/lint.ts
@@ -1,53 +1,320 @@
-import { MapDocumentId, ProfileDocumentId, Source } from '@superfaceai/parser';
-import * as nodePath from 'path';
+import {
+  formatIssues,
+  getProfileOutput,
+  parseMap,
+  parseMapId,
+  parseProfile,
+  ProfileOutput,
+  Source,
+  validateMap,
+} from '@superfaceai/parser';
+import { basename } from 'path';
 
-import { DocumentType, DOCUMENT_PARSE_FUNCTION } from '../common/document';
-import { readFile } from '../common/io';
+import {
+  composeVersion,
+  DOCUMENT_PARSE_FUNCTION,
+  DocumentType,
+  inferDocumentTypeWithFlag,
+  isMapFile,
+  isProfileFile,
+  isUnknownFile,
+  PROFILE_EXTENSIONS,
+  MAP_EXTENSIONS,
+} from '../common/document';
+import { userError } from '../common/error';
+import { DocumentTypeFlag } from '../common/flags';
+import { OutputStream, readFile } from '../common/io';
+import {
+  FileReport,
+  ProfileMapReport,
+  ReportFormat,
+} from '../common/report.interfaces';
 
-export async function getProfileDocument(path: string) {
+type ProfileDocument = ReturnType<typeof parseProfile>;
+type MapDocument = ReturnType<typeof parseMap>;
+
+export async function lintFiles(
+  files: string[],
+  outputStream: OutputStream,
+  documentTypeFlag: DocumentTypeFlag,
+  outputCounter: number,
+  outputGlue: string,
+  fn: (report: ReportFormat) => string
+): Promise<[number, number][]> {
+  return await Promise.all(
+    files.map(
+      async (file): Promise<[number, number]> => {
+        const report = await lintFile(file, documentTypeFlag);
+
+        let output = fn(report);
+        if (outputCounter > 1) {
+          output += outputGlue;
+        }
+        outputCounter -= 1;
+
+        await outputStream.write(output);
+
+        return [report.errors.length, report.warnings.length];
+      }
+    )
+  );
+}
+
+export async function lintFile(
+  path: string,
+  documentTypeFlag: DocumentTypeFlag
+): Promise<FileReport> {
+  const documenType = inferDocumentTypeWithFlag(documentTypeFlag, path);
+  if (documenType === DocumentType.UNKNOWN) {
+    throw userError('Could not infer document type', 3);
+  }
+
+  const parse = DOCUMENT_PARSE_FUNCTION[documenType];
+  const content = await readFile(path).then(f => f.toString());
+  const source = new Source(content, path);
+
+  const result: FileReport = {
+    kind: 'file',
+    path,
+    errors: [],
+    warnings: [],
+  };
+
+  try {
+    parse(source);
+  } catch (e) {
+    result.errors.push(e);
+  }
+
+  return result;
+}
+
+export function isValidHeader(
+  profile: ProfileOutput,
+  map: MapDocument,
+  mapPath: string
+): boolean {
+  const profileHeader = profile.header;
+  const mapHeader = map.header;
+  let result = true;
+
+  if (
+    profileHeader.scope !== mapHeader.profile.scope ||
+    profileHeader.name !== mapHeader.profile.name ||
+    (profileHeader.version.major !== mapHeader.profile.version.major &&
+      profileHeader.version.minor !== mapHeader.profile.version.minor)
+  ) {
+    result = false;
+  }
+
+  // fallback: compare file names
+  if (result === false) {
+    const mapIdentifier = basename(mapPath, MAP_EXTENSIONS[0]);
+    const mapId = mapIdentifier.includes('@')
+      ? mapIdentifier
+      : `${mapIdentifier}@${composeVersion(mapHeader.profile.version, true)}`;
+    const parsedMap = parseMapId(mapId);
+
+    if (parsedMap.kind === 'error') {
+      throw userError(parsedMap.message, 1);
+    }
+
+    const { name, version } = parsedMap.value;
+    if (
+      profileHeader.scope === mapHeader.profile.scope ||
+      profileHeader.name === name ||
+      (profileHeader.version.major === version.major &&
+        profileHeader.version.minor === version.minor)
+    ) {
+      result = true;
+    }
+  }
+
+  return result;
+}
+
+export async function lintMapsToProfile(
+  files: string[],
+  outputStream: OutputStream,
+  outputCounter: number,
+  outputGlue: string,
+  fn: (report: ReportFormat) => string
+): Promise<[number, number][]> {
+  const counts: [number, number][] = [];
+  const profiles = files.filter(isProfileFile);
+  const maps = files.filter(isMapFile);
+  const unknown = files.filter(isUnknownFile);
+
+  if (profiles.length === 0) {
+    throw userError('Cannot validate without profile', -1);
+  }
+  if (maps.length === 0) {
+    throw userError('Cannot validate without map', -1);
+  }
+
+  if (unknown.length > 0) {
+    for (const file of unknown) {
+      const report: FileReport = {
+        kind: 'file',
+        path: file,
+        errors: [],
+        warnings: ['Could not infer document type'],
+      };
+
+      let output = fn(report);
+      if (unknown.length > 1 && output !== '') {
+        output += outputGlue;
+      }
+
+      await outputStream.write(output);
+    }
+
+    counts.push([0, unknown.length]);
+  }
+
+  const profileDocuments: Array<ProfileDocument & { path: string }> = [];
+
+  for (const profilePath of profiles) {
+    profileDocuments.push({
+      ...(await getProfileDocument(profilePath)),
+      path: profilePath,
+    });
+  }
+
+  // loop over profiles and validate only maps that have valid header
+  for (const profile of profileDocuments) {
+    const profileOutput = getProfileOutput(profile);
+
+    for (const mapPath of maps) {
+      const map = await getMapDocument(mapPath);
+
+      if (isValidHeader(profileOutput, map, mapPath)) {
+        const result = validateMap(profileOutput, map);
+
+        const report: ProfileMapReport = result.pass
+          ? {
+              kind: 'compatibility',
+              profile: profile.path,
+              path: mapPath,
+              errors: [],
+              warnings: result.warnings ?? [],
+            }
+          : {
+              kind: 'compatibility',
+              profile: profile.path,
+              path: mapPath,
+              errors: result.errors,
+              warnings: result.warnings ?? [],
+            };
+
+        let output = fn(report);
+        if (outputCounter > 1) {
+          output += outputGlue;
+        }
+        outputCounter -= 1;
+
+        await outputStream.write(output);
+
+        counts.push([
+          result.pass ? 0 : result.errors.length,
+          result.warnings?.length ?? 0,
+        ]);
+      }
+    }
+  }
+
+  return counts;
+}
+
+export function formatHuman(
+  report: ReportFormat,
+  quiet: boolean,
+  short?: boolean,
+  _color?: boolean
+): string {
+  const REPORT_OK = '🆗';
+  const REPORT_WARN = '⚠️';
+  const REPORT_ERR = '❌';
+
+  let prefix;
+  if (report.errors.length > 0) {
+    prefix = REPORT_ERR;
+  } else if (report.warnings.length > 0) {
+    prefix = REPORT_WARN;
+  } else {
+    prefix = REPORT_OK;
+  }
+
+  const profileName =
+    'profile' in report ? `➡️ Profile:\t${report.profile}` : '';
+  let buffer = `${profileName}\n${prefix} ${report.path}\n`;
+
+  if (prefix === REPORT_WARN && quiet) {
+    return '';
+  }
+
+  if (report.kind === 'file') {
+    for (const error of report.errors) {
+      if (short) {
+        buffer += `\t${error.location.line}:${error.location.column} ${error.message}\n`;
+      } else {
+        buffer += error.format();
+      }
+    }
+    if (report.errors.length > 0 && report.warnings.length > 0) {
+      buffer += '\n';
+    }
+
+    // TODO
+    if (!quiet) {
+      for (const warning of report.warnings) {
+        if (typeof warning === 'string') {
+          buffer += `\t${warning}\n`;
+        }
+      }
+    }
+  } else {
+    buffer += formatIssues(report.errors);
+
+    if (!quiet && report.warnings.length > 1) {
+      buffer += '\n';
+    }
+
+    if (!quiet) {
+      buffer += formatIssues(report.warnings);
+      buffer += '\n';
+    }
+  }
+
+  return buffer;
+}
+
+export function formatJson(report: ReportFormat): string {
+  return JSON.stringify(report, (key, value) => {
+    if (key === 'source') {
+      return undefined;
+    }
+
+    // we are just passing the value along, nothing unsafe about that
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return value;
+  });
+}
+
+export async function getProfileDocument(
+  path: string
+): Promise<ProfileDocument> {
   const parseFunction = DOCUMENT_PARSE_FUNCTION[DocumentType.PROFILE];
   const content = (await readFile(path)).toString();
-  const source = new Source(content, nodePath.basename(path));
+  const source = new Source(content, path);
 
   return parseFunction(source);
 }
 
-export async function getMapDocument(path: string) {
+export async function getMapDocument(path: string): Promise<MapDocument> {
   const parseFunction = DOCUMENT_PARSE_FUNCTION[DocumentType.MAP];
   const content = (await readFile(path)).toString();
-  const source = new Source(content, nodePath.basename(path));
+  const source = new Source(content, path);
 
   return parseFunction(source);
-}
-
-export async function getProfileHeader(
-  path: string
-): Promise<ProfileDocumentId> {
-  const {
-    header: { scope, name, version },
-  } = await getProfileDocument(path);
-
-  return {
-    scope,
-    name,
-    version,
-  };
-}
-
-export async function getMapHeader(path: string): Promise<MapDocumentId> {
-  const {
-    header: {
-      profile: { scope, name, version },
-      provider,
-      variant,
-    },
-  } = await getMapDocument(path);
-
-  return {
-    scope,
-    name,
-    version,
-    provider,
-    variant,
-  };
 }
