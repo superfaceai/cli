82c01e7ddc36169cc02f8a46749a3f2e5ae8ba59
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-03-09 09:32:06 +0100

    feat: basic setup

---
+++ src/commands/configure.ts
@@ -0,0 +1,106 @@
+import { Command, flags } from '@oclif/command';
+import { grey, yellow } from 'chalk';
+import inquirer from 'inquirer';
+import { join as joinPath } from 'path';
+
+import {
+  META_FILE,
+  SUPERFACE_DIR,
+  validateDocumentName,
+} from '../common/document';
+import { installProvider } from '../logic/configure';
+import { initSuperface } from '../logic/init';
+import { detectSuperJson } from '../logic/install';
+
+export default class Configure extends Command {
+  static description =
+    'Initializes superface directory if needed, communicates with Superface Store API, stores provider to a local system';
+
+  static args = [
+    {
+      name: 'providerName',
+      required: true,
+      description: 'Provider name.',
+    },
+  ];
+
+  static flags = {
+    quiet: flags.boolean({
+      char: 'q',
+      description:
+        'When set to true, disables the shell echo output of init actions.',
+      default: false,
+    }),
+    force: flags.boolean({
+      char: 'f',
+      description:
+        'When set to true and when provider exists in super.json, overwrites them.',
+      default: false,
+    }),
+    //TODO: implement
+    file: flags.string({
+      char: 'p', //FIX char conflict with force - remove/rename force?
+      description: 'Path', //FIX
+      default: '',
+    }),
+    help: flags.help({ char: 'h' }),
+  };
+
+  static examples = [
+    '$ superface configure twillio',
+    '$ superface configure twillio -q',
+    '$ superface configure twillio -f',
+  ];
+
+  private warnCallback?= (message: string) => this.log(yellow(message));
+  private logCallback?= (message: string) => this.log(grey(message));
+
+  async run(): Promise<void> {
+    const { args, flags } = this.parse(Configure);
+
+    if (!validateDocumentName(args.providerName)) {
+      this.warnCallback?.('Invalid provider name');
+
+      return;
+    }
+    if (flags.quiet) {
+      this.warnCallback = undefined;
+      this.logCallback = undefined;
+    }
+
+    let superPath = await detectSuperJson(process.cwd());
+
+    if (!superPath) {
+      this.warnCallback?.("File 'super.json' has not been found.");
+
+      const response: { init: boolean } = await inquirer.prompt({
+        name: 'init',
+        message: 'Would you like to initialize new superface structure?',
+        type: 'confirm',
+      });
+
+      if (!response.init) {
+        this.exit();
+      }
+
+      this.logCallback?.(
+        "Initializing superface directory with empty 'super.json'"
+      );
+      await initSuperface(
+        './',
+        { profiles: {}, providers: {} },
+        { logCb: this.logCallback }
+      );
+      superPath = SUPERFACE_DIR;
+    }
+
+    this.logCallback?.(
+      `Installing provider to 'super.json' on path '${joinPath(superPath, META_FILE)}'`
+    );
+    await installProvider(superPath, args.providerName, {
+      logCb: this.logCallback,
+      warnCb: this.warnCallback,
+      force: flags.force,
+    });
+  }
+}
--- src/common/provider.interfaces.ts
+++ src/common/provider.interfaces.ts
@@ -1,3 +1,10 @@
+import {
+  ApiKeySecurityIn,
+  AuthSecurityType,
+  BasicAuthSecurityScheme,
+  BearerTokenSecurityScheme,
+} from './provider.enums';
+
 export interface Service {
   id: string;
   baseUrl: string;
@@ -15,9 +22,58 @@ export interface Security {
   hosts: string[];
 }
 
+export type ApiKeySecurity = {
+  id: string;
+  type: AuthSecurityType;
+  in: ApiKeySecurityIn;
+  name: string;
+};
+
+export function isApiKeySecurity(
+  auth: ApiKeySecurity | BasicAuthSecurity | BearerTokenSecurity
+): auth is ApiKeySecurity {
+  return auth.type === AuthSecurityType.API_KEY;
+}
+
+export type BasicAuthSecurity = {
+  id: string;
+  type: AuthSecurityType;
+  scheme: BasicAuthSecurityScheme;
+};
+
+export function isBasicAuthSecurity(
+  auth: ApiKeySecurity | BasicAuthSecurity | BearerTokenSecurity
+): auth is BasicAuthSecurity {
+  return (
+    auth.type === AuthSecurityType.HTTP &&
+    (auth as BasicAuthSecurity).scheme === BasicAuthSecurityScheme.BASIC
+  );
+}
+
+export type BearerTokenSecurity = {
+  id: string;
+  type: AuthSecurityType;
+  scheme: BearerTokenSecurityScheme;
+  bearerFormat?: string;
+};
+
+export function isBearerTokenSecurity(
+  auth: ApiKeySecurity | BasicAuthSecurity | BearerTokenSecurity
+): auth is BearerTokenSecurity {
+  return (
+    auth.type === AuthSecurityType.HTTP &&
+    (auth as BearerTokenSecurity).scheme === BearerTokenSecurityScheme.BEARER
+  );
+}
+//TODO: update structure
 export interface ProviderStructure {
   name: string;
   services: Service[];
   defaultService: string;
+  securitySchemes?: (
+    | ApiKeySecurity
+    | BasicAuthSecurity
+    | BearerTokenSecurity
+  )[];
   security?: Security[];
 }
