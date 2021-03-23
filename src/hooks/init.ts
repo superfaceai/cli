2b616ae8a7bb6e43b9a2cc5f0caa77ae16f73311
TheEdward162 <TheEdward162@gmail.com>
2021-03-23 19:32:06 +0100

    chore: lint:fix

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,6 +1,6 @@
 import { Hook } from '@oclif/config';
-import { VERSION as SDK_VERSION } from '@superfaceai/sdk';
 import { VERSION as PARSER_VERSION } from '@superfaceai/parser';
+import { VERSION as SDK_VERSION } from '@superfaceai/sdk';
 
 export const hook: Hook<'init'> = async function (_options) {
   this.config.userAgent += ` (with @superfaceai/sdk/${SDK_VERSION}, @superfaceai/parser/${PARSER_VERSION})`;
