fb5ec78c693628231ed00d33783b1cde022f5881
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-05-03 08:21:40 +0200

    chore: export generate types functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-var-requires
 const packageJson = require('../package.json');
 
+export * from './logic';
 export { run } from '@oclif/command';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
