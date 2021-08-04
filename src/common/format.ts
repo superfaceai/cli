adb33a388df3e97995484467c5ba3faa6489429e
Martin Albert <martin.albert.187@gmail.com>
2021-08-04 15:56:06 +0200

    feat: add function to remove ISO date from errors

--- src/common/format.ts
+++ src/common/format.ts
@@ -1,3 +1,5 @@
+const ISO_DATE_REGEX = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)/gm;
+
 export function formatWordPlurality(num: number, word: string): string {
   if (num === 1) {
     return `${num} ${word}`;
@@ -5,3 +7,6 @@ export function formatWordPlurality(num: number, word: string): string {
     return `${num} ${word}s`;
   }
 }
+
+export const removeTimestamp = (payload: string): string =>
+  payload.replace(ISO_DATE_REGEX, '');
