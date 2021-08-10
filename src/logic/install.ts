f8e4dda5f7dbdcce6ee222b2c9be03600758b99c
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-08-10 07:21:02 +0200

    chore: clean up

--- src/logic/install.ts
+++ src/logic/install.ts
@@ -404,8 +404,6 @@ export async function getProfileFromStore(
   options?.logCb?.(`\nFetching profile ${profileId} from the Store`);
 
   try {
-    options?.logCb?.(`BEFORE GET Profile Info ${profileId}`);
-
     info = await fetchProfileInfo(profileId);
     options?.logCb?.(`GET Profile Info ${profileId}`);
 
