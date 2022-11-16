d52ff3fe295548d242d25a44b25869e3c3807ca2
Jakub Vacek <vacjak@seznam.cz>
2022-11-16 15:51:14 +0100

    fix: indent not intent

--- src/templates/prepared-map/map-templates/list.ts
+++ src/templates/prepared-map/map-templates/list.ts
@@ -1,6 +1,6 @@
-export default `[{{newLine (inc intent 2) }}` +
+export default `[{{newLine (inc indent 2) }}` +
   `{{#ifeq modelType "Scalar"}}{{>Scalar use=":" }},{{/ifeq}}` +
   `{{#ifeq modelType "Enum"}}{{>Enum use=":" }},{{/ifeq}}` +
-  `{{#ifeq modelType "Object"}}{{>Object this use=":" intent=(inc intent 2) }},{{/ifeq}}` +
-  `{{#ifeq modelType "List"}}{{>Array this use=":" intent= (inc intent 2) }},{{/ifeq}}` +
-  `{{newLine intent}}]`;
+  `{{#ifeq modelType "Object"}}{{>Object this use=":" indent=(inc indent 2) }},{{/ifeq}}` +
+  `{{#ifeq modelType "List"}}{{>Array this use=":" indent= (inc indent 2) }},{{/ifeq}}` +
+  `{{newLine indent}}]`;
--- src/templates/prepared-map/map-templates/object.ts
+++ src/templates/prepared-map/map-templates/object.ts
@@ -1,8 +1,8 @@
 export default `{{openObject}}` +
-  `{{#each fields}}{{newLine (inc ../intent 2) }}` +
+  `{{#each fields}}{{newLine (inc ../indent 2) }}` +
   `{{#ifeq model.modelType "Scalar"}}{{>Scalar this scalarType=model.scalarType use=../use}}{{/ifeq}}` +
   `{{#ifeq model.modelType "Enum"}}{{>Enum use=../use}}{{/ifeq}}` +
-  `{{#ifeq model.modelType "Object"}}{{fieldName}}{{../use}} {{>Object model use=":" intent= (inc ../intent 2) }},{{/ifeq}}` +
-  `{{#ifeq model.modelType "List"}}{{fieldName}}{{../use}} {{>Array model.model use=":" intent= (inc ../intent 2) }},{{/ifeq}}` +
-  `{{/each}}{{newLine intent}}` +
+  `{{#ifeq model.modelType "Object"}}{{fieldName}}{{../use}} {{>Object model use=":" indent= (inc ../indent 2) }},{{/ifeq}}` +
+  `{{#ifeq model.modelType "List"}}{{fieldName}}{{../use}} {{>Array model.model use=":" indent= (inc ../indent 2) }},{{/ifeq}}` +
+  `{{/each}}{{newLine indent}}` +
   `{{closeObject}}`;
