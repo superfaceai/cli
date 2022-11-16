a9e81b858668489c5082a012417cd6a53c652933
Jakub Vacek <vacjak@seznam.cz>
2022-11-16 15:41:01 +0100

    fix: extra space
    Co-authored-by: Radek Kyselý <radek.kysely@superface.ai>

--- src/templates/prepared-map/mock-map-templates/result.ts
+++ src/templates/prepared-map/mock-map-templates/result.ts
@@ -1,2 +1,2 @@
-export default `return map result {{#ifeq kind "object"}}{{>Object use=" =" intent=0}}{{/ifeq}}{{#ifeq kind "array"}}{{>Array use=":" intent=0}}{{/ifeq}}
+export default `return map result {{#ifeq kind "object"}}{{>Object use="=" intent=0}}{{/ifeq}}{{#ifeq kind "array"}}{{>Array use=":" intent=0}}{{/ifeq}}
 `;
