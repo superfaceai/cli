0fa2f16c9ea196180bd65579cddf45053992ce9d
Jakub Vacek <vacjak@seznam.cz>
2022-12-01 07:35:53 +0100

    chore: align with station conventions

--- src/templates/profile.ts
+++ src/templates/profile.ts
@@ -33,6 +33,8 @@ export function completeProfile({
   const id = ProfileId.fromScopeName(scope, name);
   const formattedName = startCase(id.id);
 
+  const error = errorModel(scope, usecaseNames.length > 1);
+
   return [
     `"""
 ${formattedName}
@@ -41,15 +43,12 @@ TODO: What "${formattedName}" does
 name = "${id.id}"
 version = "${version}"
 // Comlink Profile specification: https://superface.ai/docs/comlink/profile`,
-    ...usecaseNames.map(u => usecase(u, scope)),
-    errorModel(scope),
+    ...usecaseNames.map(u => usecase(u, error.statement)),
+    error.namedModel !== undefined ? error.namedModel : '',
   ].join('\n');
 }
 
-function usecase(useCaseName: string, scope?: string): string {
-  const errorName =
-    scope !== undefined ? `${startCase(scope, '')}Error` : `DomainError`;
-
+function usecase(useCaseName: string, errorStatement: string): string {
   const formattedName = startCase(useCaseName);
 
   return `
@@ -57,17 +56,24 @@ function usecase(useCaseName: string, scope?: string): string {
 ${formattedName}
 TODO: What "${formattedName}" does
 """
-usecase ${useCaseName} {
+usecase ${useCaseName} unsafe { // change safety to \`safe\` or \`idempotent\` or \`unsafe\`
   input {
-    "field title"
+    """
+    Foo
+    TODO: description of "foo"
+    """
     foo! string 
   }
 
   result {
+    """
+    Bar
+    TODO: description of "bar"
+    """
     bar string
   }
 
-  error ${errorName}
+  error ${errorStatement}
 
   example success {
     input {
@@ -90,22 +96,35 @@ usecase ${useCaseName} {
 }`;
 }
 
-function errorModel(scope?: string): string {
-  const errorName =
-    scope !== undefined ? `${startCase(scope, '')}Error` : `DomainError`;
+function errorModel(
+  scope: string | undefined,
+  reuseModel: boolean
+): { statement: string; namedModel?: string } {
+  const definition = `{
+    """
+    Title
+    A short, human-readable summary of the problem type.
+    """
+    title! string!
+  
+    """
+    Detail
+    A human-readable explanation specific to this occurrence of the problem.
+    """
+    detail string! 
+  }`;
 
-  return `
-model ${errorName} {
-  """
-  Title
-  A short, human-readable summary of the problem type.
-  """
-  title! string!
+  if (reuseModel) {
+    const errorName =
+      scope !== undefined ? `${startCase(scope, '')}Error` : `DomainError`;
 
-  """
-  Detail
-  A human-readable explanation specific to this occurrence of the problem.
-  """
-  detail string! 
-}`;
+    return {
+      statement: errorName,
+      namedModel: `model ${errorName}` + definition,
+    };
+  }
+
+  return {
+    statement: definition,
+  };
 }
