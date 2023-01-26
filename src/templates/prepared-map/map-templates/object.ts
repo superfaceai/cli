e5deab4f50c575d22b6d0b13ef4e858a4cc2d440
Jakub Vacek <vacjak@seznam.cz>
2023-01-26 08:43:28 +0100

    fix: map generation array of enums

--- src/templates/prepared-map/map-templates/object.ts
+++ src/templates/prepared-map/map-templates/object.ts
@@ -2,8 +2,8 @@ export default `{{openObject}}` +
   `{{#each fields}}{{newLine (inc ../indent 2) }}` +
   `{{#unless model }}{{>Unknown this use=../use}}{{/unless}}` +
   `{{#if model }}{{#ifeq model.modelType "Scalar"}}{{>Scalar this scalarType=model.scalarType use=../use}}{{/ifeq}}` +
-  `{{#ifeq model.modelType "Enum"}}{{>Enum use=../use}}{{/ifeq}}` +
-  `{{#ifeq model.modelType "Object"}}{{fieldName}}{{../use}} {{>Object model use=":" indent= (inc ../indent 2) }},{{/ifeq}}` +
-  `{{#ifeq model.modelType "List"}}{{fieldName}}{{../use}} {{>Array model use=":" indent= (inc ../indent 2) }},{{/ifeq}}{{/if}}` +
+  `{{#ifeq model.modelType "Enum"}}{{#if fieldName}}{{fieldName}}{{/if}}{{#unless fieldName}}unknonwn{{/unless}}{{../use}} {{>Enum }}{{/ifeq}}` +
+  `{{#ifeq model.modelType "Object"}}{{#if fieldName}}{{fieldName}}{{/if}}{{#unless fieldName}}unknonwn{{/unless}}{{../use}} {{>Object model use=":" indent= (inc ../indent 2) }},{{/ifeq}}` +
+  `{{#ifeq model.modelType "List"}}{{#if fieldName}}{{fieldName}}{{/if}}{{#unless fieldName}}unknonwn{{/unless}}{{../use}} {{>Array model use=":" indent= (inc ../indent 2) }},{{/ifeq}}{{/if}}` +
   `{{/each}}{{newLine indent}}` +
   `{{closeObject}}`;
