f7fafa081822587bffd26d0eae3b0c0bad8789df
Jakub Vacek <vacjak@seznam.cz>
2023-06-13 12:48:30 +0200

    feat: x file structure

---
+++ src/common/file-structure.ts
@@ -0,0 +1,51 @@
+import { join, resolve } from 'path';
+
+const DEFAULT_SUPERFACE_DIR = 'superface';
+
+const PROFILE_EXTENSION = '.profile';
+
+const PROVIDER_EXTENSION = '.provider.json';
+
+const MAP_EXTENSION = '.map.js';
+
+export function buildSuperfaceDirPath(): string {
+  return join(resolve(process.cwd()), DEFAULT_SUPERFACE_DIR);
+}
+
+export function buildProfilePath(profileId: string): string {
+  return join(
+    resolve(process.cwd()),
+    DEFAULT_SUPERFACE_DIR,
+    `${profileId}${PROFILE_EXTENSION}`
+  );
+}
+
+export function buildProviderPath(providerName: string): string {
+  return join(
+    resolve(process.cwd()),
+    DEFAULT_SUPERFACE_DIR,
+    `${providerName}${PROVIDER_EXTENSION}`
+  );
+}
+
+export function buildMapPath(profileId: string, providerName: string): string {
+  return join(
+    resolve(process.cwd()),
+    DEFAULT_SUPERFACE_DIR,
+    `${profileId}.${providerName}${MAP_EXTENSION}`
+  );
+}
+
+export function buildRunFilePath(
+  profileId: string,
+  providerName: string,
+  language: 'JS' | 'Python'
+): string {
+  const extension = language === 'JS' ? '.mjs' : '.py';
+
+  return join(
+    resolve(process.cwd()),
+    DEFAULT_SUPERFACE_DIR,
+    `${profileId}.${providerName}${extension}`
+  );
+}
