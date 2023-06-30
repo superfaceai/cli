840c362edc99f4716d694b65551bdffb4e0880a1
Jakub Vacek <vacjak@seznam.cz>
2023-06-30 11:54:53 +0200

    fix: do not store polling info

--- src/common/ux.ts
+++ src/common/ux.ts
@@ -2,11 +2,8 @@ import { green, red, yellow } from 'chalk';
 import type { Spinner } from 'nanospinner';
 import { createSpinner } from 'nanospinner';
 
-// TODO: This could be singleton that is injectet into each command class as dependency and there is a helper methot to get instance from user error handler - on error we need to call fail method
 export class UX {
   private static instance: UX | undefined;
-
-  private log: string[] = [];
 
   private readonly spinner: Spinner;
 
@@ -40,8 +37,7 @@ export class UX {
     if (!text.endsWith('\n')) {
       text += '\n';
     }
-    this.log.push(text);
-    this.spinner.update({ text: this.log.join('\n') });
+    this.spinner.update({ text });
   }
 
   public warn(text: string): void {
