d43884a4e4988c596d58fb08847f6979875d7149
Jakub Vacek <vacjak@seznam.cz>
2023-07-10 08:45:25 +0200

    feat: save remote provider

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,22 +1,17 @@
-import type { ProviderJson } from '@superfaceai/ast';
-import {
-  AssertionError,
-  assertProviderJson,
-  isValidProviderName,
-} from '@superfaceai/ast';
 import { basename } from 'path';
 
 import type { Flags } from '../common/command.abstract';
 import { Command } from '../common/command.abstract';
-import { stringifyError, type UserError } from '../common/error';
-import { buildProfilePath, buildProviderPath } from '../common/file-structure';
-import { exists, readFile } from '../common/io';
+import type { UserError } from '../common/error';
+import { buildProfilePath } from '../common/file-structure';
+import { SuperfaceClient } from '../common/http';
+import { exists } from '../common/io';
 import type { ILogger } from '../common/log';
 import { OutputStream } from '../common/output-stream';
 import { ProfileId } from '../common/profile';
+import { resolveProviderJson } from '../common/provider';
 import { UX } from '../common/ux';
 import { newProfile } from '../logic/new';
-import { SuperfaceClient } from '../common/http';
 
 const MAX_PROMPT_LENGTH = 200;
 
@@ -79,6 +74,7 @@ export default class New extends Command {
 
     const resolvedProviderJson = await resolveProviderJson(providerName, {
       userError,
+      client: SuperfaceClient.getClient(),
     });
 
     if (resolvedProviderJson.source === 'local') {
@@ -149,130 +145,4 @@ function checkPrompt(
       1
     );
   }
-}
-
-// TODO: move to common
-export async function resolveProviderJson(
-  providerName: string | undefined,
-  { userError }: { userError: UserError }
-): Promise<
-  {
-    providerJson: ProviderJson;
-  } & ({ source: 'local'; path: string } | { source: 'remote' })
-> {
-  if (providerName === undefined) {
-    throw userError(
-      'Missing provider name. Please provide it as first argument.',
-      1
-    );
-  }
-
-  if (!isValidProviderName(providerName)) {
-    throw userError('Invalid provider name', 1);
-  }
-
-  let resolvedProviderJson: ProviderJson | undefined;
-  let source: 'local' | 'remote' = 'local';
-  let path: string;
-
-  if (!(await exists(buildProviderPath(providerName)))) {
-    const client = SuperfaceClient.getClient();
-
-    try {
-      const providerResponse = await client.fetch(
-        `/authoring/providers/${providerName}`,
-        {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-          },
-        }
-      );
-
-      if (providerResponse.status === 200) {
-        try {
-          resolvedProviderJson = assertProviderJson(
-            (await providerResponse.json()) as ProviderJson
-          );
-        } catch (e) {
-          if (e instanceof AssertionError) {
-            throw userError(`Invalid provider.json file. ${e.message}`, 1);
-          }
-          throw userError(`Invalid provider.json file.`, 1);
-        }
-      } else if (providerResponse.status === 404) {
-        throw userError(
-          `Provider ${providerName} does not exist both locally and remotely. Make sure to run "sf prepare" before running this command.`,
-          1
-        );
-      } else {
-        throw userError(
-          `Failed to fetch provider.json file from Superface API. ${stringifyError(
-            providerResponse
-          )}`,
-          1
-        );
-      }
-    } catch (e) {
-      throw userError(
-        `Failed to fetch provider.json file from Superface API. ${stringifyError(
-          e
-        )}`,
-        1
-      );
-    }
-
-    await OutputStream.writeOnce(
-      buildProviderPath(resolvedProviderJson.name),
-      JSON.stringify(resolvedProviderJson, null, 2)
-    );
-  } else {
-    path = buildProviderPath(providerName);
-    const providerJsonFile = await readFile(path, 'utf-8');
-    let providerJson: ProviderJson;
-    try {
-      providerJson = JSON.parse(providerJsonFile) as ProviderJson;
-    } catch (e) {
-      throw userError(`Invalid provider.json file.`, 1);
-    }
-
-    try {
-      resolvedProviderJson = assertProviderJson(providerJson);
-    } catch (e) {
-      if (e instanceof AssertionError) {
-        throw userError(`Invalid provider.json file. ${e.message}`, 1);
-      }
-      throw userError(`Invalid provider.json file.`, 1);
-    }
-  }
-
-  if (providerName !== resolveProviderJson.name) {
-    throw userError(
-      `Provider name in provider.json file does not match provider name in command.`,
-      1
-    );
-  }
-
-  if (
-    resolvedProviderJson.services.length === 1 &&
-    resolvedProviderJson.services[0].baseUrl.includes('TODO')
-  ) {
-    throw userError(
-      `Provider.json file is not properly configured. Please make sure to replace 'TODO' in baseUrl with the actual base url of the API.`,
-      1
-    );
-  }
-
-  if (source === 'local') {
-    return {
-      providerJson: resolvedProviderJson,
-      source,
-      path: path!,
-    };
-  }
-
-  return {
-    providerJson: resolvedProviderJson,
-    source,
-  };
 }
---
+++ src/common/provider.test.ts
@@ -0,0 +1,252 @@
+import type { ProviderJson } from '@superfaceai/ast';
+import type { ServiceClient } from '@superfaceai/service-client';
+
+import { mockResponse } from '../test/utils';
+import { createUserError, stringifyError } from './error';
+import { exists, readFile } from './io';
+import { OutputStream } from './output-stream';
+import { resolveProviderJson } from './provider';
+
+jest.mock('./io');
+jest.mock('../common/output-stream');
+
+const mockFetch = jest.fn();
+
+describe('resolveProviderJson', () => {
+  const originalWriteOnce = OutputStream.writeOnce;
+
+  let mockWriteOnce: jest.Mock;
+  const userError = createUserError(false);
+
+  let providerJson: ProviderJson;
+
+  const client = { fetch: mockFetch } as unknown as jest.Mocked<ServiceClient>;
+
+  beforeAll(() => {
+    // Mock static side of OutputStream
+    mockWriteOnce = jest.fn();
+    OutputStream.writeOnce = mockWriteOnce;
+  });
+
+  beforeEach(() => {
+    providerJson = {
+      name: 'test',
+      services: [
+        {
+          id: 'test',
+          baseUrl: 'https://test.com',
+        },
+      ],
+      defaultService: 'test',
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    // Restore static side of OutputStream
+    OutputStream.writeOnce = originalWriteOnce;
+  });
+
+  it('should throw error when provider name is missing', async () => {
+    await expect(
+      resolveProviderJson(undefined, {
+        userError,
+        client,
+      })
+    ).rejects.toThrowError(
+      'Missing provider name. Please provide it as first argument.'
+    );
+  });
+
+  it('should throw error when provider name is invalid', async () => {
+    await expect(
+      resolveProviderJson('test!', {
+        userError,
+        client,
+      })
+    ).rejects.toThrowError('Invalid provider name');
+  });
+
+  it('should throw error when provider name does not match', async () => {
+    jest.mocked(exists).mockResolvedValue(true);
+    jest.mocked(readFile).mockResolvedValue(
+      JSON.stringify({
+        ...providerJson,
+        name: 'other-test',
+      })
+    );
+
+    await expect(
+      resolveProviderJson('test', {
+        userError,
+        client,
+      })
+    ).rejects.toThrowError(
+      'Provider name in provider.json file does not match provider name in command.'
+    );
+  });
+
+  describe('when provider json is remote', () => {
+    it('should return provider json', async () => {
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(
+        mockResponse(200, 'ok', undefined, { definition: providerJson })
+      );
+
+      const result = await resolveProviderJson('test', {
+        userError,
+        client,
+      });
+
+      expect(result).toEqual({
+        providerJson,
+        source: 'remote',
+      });
+    });
+
+    it('should throw error when provider json does not exist', async () => {
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(mockResponse(404, 'not found'));
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError(
+        'Provider test does not exist both locally and remotely. Make sure to run "sf prepare" before running this command.'
+      );
+    });
+
+    it('should throw error when server returns unexpected data', async () => {
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(
+        mockResponse(200, 'ok', undefined, { test: 'test' })
+      );
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError('Invalid provider.json - invalid JSON');
+    });
+
+    it('should throw error when provider json is invalid JSON', async () => {
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(mockResponse(200, 'ok', undefined));
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError('Invalid provider.json - invalid JSON');
+    });
+
+    it('should throw error when provider json is invalid', async () => {
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(
+        mockResponse(200, 'ok', undefined, { definition: { test: 'test' } })
+      );
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError(
+        'Invalid provider.json. $: must have required property "defaultService"'
+      );
+    });
+
+    it('should throw error when server returns 500', async () => {
+      const errorResponse = mockResponse(500, 'error', undefined, {
+        message: 'test',
+      });
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockResolvedValue(errorResponse);
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError(
+        userError(
+          `Failed to fetch provider.json file from Superface API. ${stringifyError(
+            errorResponse
+          )}`,
+          1
+        )
+      );
+    });
+
+    it('should throw error when fetching fails', async () => {
+      const error = new Error('test');
+      jest.mocked(exists).mockResolvedValue(false);
+      mockFetch.mockRejectedValue(error);
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError(
+        userError(
+          `Failed to fetch provider.json file from Superface API. ${stringifyError(
+            error
+          )}`,
+          1
+        )
+      );
+    });
+  });
+
+  describe('when provider json is local', () => {
+    it('should return provider json', async () => {
+      jest.mocked(exists).mockResolvedValue(true);
+      jest.mocked(readFile).mockResolvedValue(JSON.stringify(providerJson));
+
+      const result = await resolveProviderJson('test', {
+        userError,
+        client,
+      });
+
+      expect(result).toEqual({
+        providerJson,
+        source: 'local',
+        path: expect.stringContaining('/superface/test.provider.json'),
+      });
+    });
+
+    it('should throw error when provider json is not valid JSON', async () => {
+      jest.mocked(exists).mockResolvedValue(true);
+      jest.mocked(readFile).mockResolvedValue('invalid');
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError('Invalid provider.json file - invalid JSON');
+    });
+
+    it('should throw error when provider json is invalid', async () => {
+      jest.mocked(exists).mockResolvedValue(true);
+      jest.mocked(readFile).mockResolvedValue(JSON.stringify({ test: 'test' }));
+
+      await expect(
+        resolveProviderJson('test', {
+          userError,
+          client,
+        })
+      ).rejects.toThrowError(
+        'Invalid provider.json file. $: must have required property "defaultService"'
+      );
+    });
+  });
+});
