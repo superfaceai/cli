90f78f91087046d82b2671d5a5663105d8c19b9d
Radek Kyselý <radek@kysely.org>
2023-07-13 11:02:53 +0200

    feat(boilerplate): Adding OneSDK token constants

---
+++ src/logic/application-code/dotenv/onesdk-token.ts
@@ -0,0 +1,3 @@
+export const ONESDK_TOKEN_ENV = 'SUPERFACE_ONESDK_TOKEN';
+export const ONESDK_TOKEN_COMMENT = 'OneSDK token lets your monitor your usage out-of-the-box at https://superface.ai/app'
+export const ONESDK_TOKEN_UNAVAILABLE_COMMENT = 'Set your OneSDK token to monitor your usage out-of-the-box. Get yours at https://superface.ai/app'
