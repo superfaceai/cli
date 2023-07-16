c7e0e354f52748a017b9f0830490e97e6979be8c
Radek Kyselý <radek@kysely.org>
2023-07-16 16:39:11 +0200

    chore(boilerplate): Refactored dotenv test

--- src/logic/application-code/dotenv/dotenv.test.ts
+++ src/logic/application-code/dotenv/dotenv.test.ts
@@ -33,8 +33,8 @@ MY_PROVIDER_TOKEN=
 `;
 
 describe('createNewDotenv', () => {
-  describe('when there is no previous .env', () => {
-    it('creates valid .env when no token, no parameters or security schemes are given', () => {
+  describe('OneSDK token', () => {
+    it('when no token is given, adds empty token env', () => {
       const result = createNewDotenv({ providerName: PROVIDER_NAME });
 
       expect(result).toStrictEqual({
@@ -45,7 +45,23 @@ SUPERFACE_ONESDK_TOKEN=
       });
     });
 
-    it('creates valid .env when valid token but no parameters or security schemes are given', () => {
+    it('when a token is given, adds pre-filled token env', () => {
+      const result = createNewDotenv({
+        providerName: PROVIDER_NAME,
+        token: TOKEN,
+      });
+
+      expect(result).toStrictEqual({
+        content: `# The token for monitoring your Comlinks at https://superface.ai
+SUPERFACE_ONESDK_TOKEN=sfs_b31314b7fc8...8ec1930e
+`,
+        newEmptyEnvVariables: [],
+      });
+    });
+  });
+
+  describe('when there is no previous .env', () => {
+    it('creates valid .env when no parameters or security schemes are given', () => {
       const result = createNewDotenv({
         providerName: PROVIDER_NAME,
         token: TOKEN,
