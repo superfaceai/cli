6791c29d277a287635285194268754955f00c05a
Jakub Vacek <vacjak@seznam.cz>
2023-08-28 16:50:55 +0200

    chore: clean up document

--- src/common/common.test.ts
+++ src/common/common.test.ts
@@ -1,6 +1,6 @@
 import { DocumentType, inferDocumentType } from '@superfaceai/ast';
 
-import { inferDocumentTypeWithFlag } from './document';
+// import { inferDocumentTypeWithFlag } from './document';
 
 describe('inferDocumentType()', () => {
   it('infers map type', () => {
@@ -15,44 +15,5 @@ describe('inferDocumentType()', () => {
     expect(inferDocumentType('test.supr.face')).toEqual(DocumentType.UNKNOWN);
     expect(inferDocumentType('test.slang')).toEqual(DocumentType.UNKNOWN);
     expect(inferDocumentType('test')).toEqual(DocumentType.UNKNOWN);
-  });
-});
-
-describe('inferDocumentTypeWithFlag()', () => {
-  it('infers map type when flag is map', () => {
-    expect(inferDocumentTypeWithFlag('map')).toEqual(DocumentType.MAP);
-    expect(inferDocumentTypeWithFlag('map', 'test.suma')).toEqual(
-      DocumentType.MAP
-    );
-    expect(inferDocumentTypeWithFlag('map', 'test.supr')).toEqual(
-      DocumentType.MAP
-    );
-  });
-
-  it('infers profile type when flag is profile', () => {
-    expect(inferDocumentTypeWithFlag('profile')).toEqual(DocumentType.PROFILE);
-    expect(inferDocumentTypeWithFlag('profile', 'test.suma')).toEqual(
-      DocumentType.PROFILE
-    );
-    expect(inferDocumentTypeWithFlag('profile', 'test.supr')).toEqual(
-      DocumentType.PROFILE
-    );
-  });
-
-  it('infers unknown when flag is auto and path is not passed', () => {
-    expect(inferDocumentTypeWithFlag('auto')).toEqual(DocumentType.UNKNOWN);
-  });
-
-  it('infers from path when flag is auto', () => {
-    expect(inferDocumentTypeWithFlag('auto', 'test.suma')).toEqual(
-      DocumentType.MAP
-    );
-    expect(inferDocumentTypeWithFlag('auto', 'test.supr')).toEqual(
-      DocumentType.PROFILE
-    );
-
-    expect(inferDocumentTypeWithFlag('auto', 'test')).toEqual(
-      DocumentType.UNKNOWN
-    );
   });
 });
